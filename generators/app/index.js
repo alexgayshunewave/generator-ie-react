6de63554ada6be8c1d62f7118246c9e83704ec4a
Xian Yang Wong <xianyang.wong@ie.com.au>
2019-04-26 13:27:15 +1000

    Generator update with create-react-app and redux dependencies

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,20 +1,27 @@
 const Generator = require('yeoman-generator');
-
+const yosay = require('yosay')
+const _ = require('lodash')
 module.exports = class extends Generator {
     constructor(args, opts) {
         super(args, opts);
-        this.option('typescript');
+        this.option('projectName');
     }
+    welcome() {
+        this.log(
+          yosay(
+            "Generator for a react app using typescript, redux and other goodies."+
+            "\n\nBrought to you by --- IE"
+          )
+        )
+      }
 
     createReactApp() {
         this.log("Starting the IE React Generator");
-        return this.prompt([{
-            type: 'confirm',
-            name: 'typescript',
-            message: 'Do you want this project to use TypeScript?',
-            default: true
-        }]).then((answers) => {
-            this.log(answers);
-        });
+        var done = this.async();
+        return this.spawnCommand('npx', ['create-react-app', '.', '--typescript']).on('close', done);
+    }
+
+    installExtraDependencies() {
+        this.yarnInstall(['@types/react-redux', 'react-redux', 'redux']);
     }
 };
